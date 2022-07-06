e339ffb035d3c3fdc090009709674d92d809cad5
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-07-06 19:37:49 +0300

    fix style form

--- src/components/Element/LoginForm/LoginForm.jsx
+++ src/components/Element/LoginForm/LoginForm.jsx
@@ -5,10 +5,6 @@ import { LoginButton, RegButton } from "../../UI/Button/Button";
 import "./LoginForm.scss"
 import {IconLock, IconMail} from "../../../assets/img/index.js"
 
-
-
-
-
     // <form className="login-form">
     // <div className="login-form__name form-email">
     //     <label className="form-name__label" for="fullname">
@@ -40,14 +36,14 @@ export const LoginForm = () => {
     <>
      <form className="login-form">
       <div className="login-form__email form-email">
-        <label className="form-email__label" for="email">
+        <label className="form-email__label" htmlFor="email">
           Email:
           <input type="email" className="form-email__input" id="email" name="email" placeholder="Type your e-mail" required />
         </label>
         <div className="form-email__img"><img src={IconMail} alt="" /></div>
       </div>
       <div className="login-form__password form-password">
-        <label className="form-password__label" for="password">
+        <label className="form-password__label" htmlFor="password">
           Password:
           <input type="password" className="form-password__input" id="password" name="password" placeholder="Type your password" required />
         </label>
@@ -55,7 +51,7 @@ export const LoginForm = () => {
       </div>
       <div className="login-form__checkbox form-checkbox">
         <input className="form-checkbox__input" type="checkbox" id="check-login" name="check-login" />
-        <label className="form-checkbox__label" for="check-login">Keep me logged in</label>
+        <label className="form-checkbox__label" htmlFor="check-login">Keep me logged in</label>
       </div>
       <LoginButton />
     </form>
