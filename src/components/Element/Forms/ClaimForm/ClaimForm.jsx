f5b513ccc37f92b7f6273e1d8642a920f4a5abc8
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-07-20 13:29:07 +0300

    new parameters in the form, fixed display of elements in the select, check for admin

--- src/components/Element/Forms/ClaimForm/ClaimForm.jsx
+++ src/components/Element/Forms/ClaimForm/ClaimForm.jsx
@@ -1,67 +1,126 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
+import { dataBase } from "../../../../api/api";
 import "../../../UI/Button/Button.scss";
-import {Button} from "../../../UI/Button/Button";
-import Input from "../../../UI/Input.jsx";
-import {Select} from "./Select.jsx";
-import {Description} from "./Description";
-import {createClaim} from "../../../../store/slices/claimSlice";
-import {useInput} from "../../../../hooks/useValidate/useValidation";
+import { Button } from "../../../UI/Button/Button";
+import { Input } from "../../../UI/Input.jsx";
+import { Select } from "./Select.jsx";
+import { Description } from "./Description";
+import { createClaim } from "../../../../store/slices/claimSlice";
+import { useInput } from "../../../../hooks/useValidate/useValidation";
 
-export const ClaimForm = () => {
-	const [form, setForm] = useState({title: "", type: "", description: ""});
-	const title = useInput("", {isEmpty: true, minLength: 2});
-	const type = useInput("", {isEmpty: true});
-	const description = useInput("", {isEmpty: true, minLength: 2});
+export const ClaimForm = ({ formData }) => {
+  const [form, setForm] = useState({ title: "", type: "", description: "" });
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const [types, setTypes] = useState([]);
+  const [statuses, setStatuses] = useState([]);
 
-	const onCreatedClick = () => {
-		dispatch(createClaim(form));
-		setForm({title: "", type: "", description: "", status: 'new'});
-		navigate("/");
-	};
+  const title = useInput("", { isEmpty: true, minLength: 2 });
+  const type = useInput("", { isEmpty: true });
+  const description = useInput("", { isEmpty: true, minLength: 2 });
 
-	return (
-		<>
-			{title.isDirty && title.isEmpty && <span style={{color: "#CC1F1F"}}>Field cannot be empty</span>}
-			<Input
-				label="Title"
-				className="form-title__input"
-				placeholder="Type claim title"
-				labelClassName="form-title__label"
-				wrapperLabel="form-title"
-				onChangeTitle={(e) => setForm({...form, title: e.target.value})}
-				value={form.title}
-				onChangeValid={(e) => title.onChange(e)}
-				onBlur={(e) => title.onBlur(e)}
-			/>
+  const role = useSelector((state) => state.user.user.role);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-			{type.isDirty && type.isEmpty && <span style={{color: "#CC1F1F"}}>Field cannot be empty</span>}
-			<Select label="Type" className="form-type__select" onChangeType={(e) => setForm({...form, type: e.target.value})} value={form.type} onChangeValid={(e) => type.onChange(e)} onBlur={(e) => type.onBlur(e)} />
+  useEffect(() => {
+    if (formData) {
+      setForm({ title: formData.title, description: formData.description, type: formData.type.slug, status: formData.status.slug, _id: formData._id });
+    }
+    dataBase()
+      .get("/types")
+      .then(({ data }) => setTypes(data));
+    dataBase()
+      .get("/status")
+      .then(({ data }) => setStatuses(data));
+  }, []);
 
-			{description.isDirty && description.isEmpty && <span style={{color: "#CC1F1F"}}>Field cannot be empty</span>}
-			<Description
-				label="Description"
-				placeholder="Type claim description"
-				className="form-description__textarea"
-				onChangeDescription={(e) => setForm({...form, description: e.target.value})}
-				value={form.description}
-				onChangeValid={(e) => description.onChange(e)}
-				onBlur={(e) => description.onBlur(e)}
-			/>
-			<Button value="Cansel" type="cansel" />
-			<Button
-				value="Create"
-				type="create"
-				disabled={!title.inputValid || !type.inputValid || !description.inputValid}
-				onCreatedClick={() => {
-					onCreatedClick(form);
-				}}
-			/>
-		</>
-	);
+  const onCreatedClick = () => {
+    if (!title.inputValid || !type.inputValid || !description.inputValid) {
+      console.log("error");
+    } else {
+      dataBase()
+        .post("/claim", {
+          title: title.value,
+          description: description.value,
+          type: type.value,
+        })
+        .then((responce) => {
+          if (responce && responce.status === 200) {
+            const resClaim = {
+              id: responce.data.id,
+              title: responce.data.title,
+              create: responce.data.create,
+              type: responce.data.type.slug,
+              status: responce.data.status.slug,
+              description: responce.data.description,
+            };
+            dispatch(createClaim(resClaim));
+            navigate("/");
+          }
+        })
+        .catch((err) => { });
+    }
+
+    const addStatus = { ...form, status: "new" };
+    dispatch(createClaim(addStatus));
+    setForm({ title: "", type: "", description: "" });
+  };
+
+  return (
+    <>
+      {title.isDirty && title.isEmpty && <span style={{ color: "#CC1F1F" }}>Field cannot be empty</span>}
+
+      <Input
+        label="Title"
+        className="form-title__input"
+        placeholder="Type claim title"
+        labelClassName="form-title__label"
+        wrapperLabel="form-title"
+        onChangeTitle={(e) => setForm({ ...form, title: e.target.value })}
+        value={form.title}
+        onChangeValid={(e) => title.onChange(e)}
+        onBlur={(e) => title.onBlur(e)}
+        disabled={role.slug === "admin" ? true : false}
+      />
+
+      {type.isDirty && type.isEmpty && <span style={{ color: "#CC1F1F" }}>Field cannot be empty</span>}
+      <Select label="Type"
+        className="form-type__select"
+        onChangeType={(e) => setForm({ ...form, type: e.target.value })}
+        value={form.type}
+        onChangeValid={(e) => type.onChange(e)}
+        onBlur={(e) => type.onBlur(e)}
+        options={types}
+        disabled={role.slug === "admin" ? true : false}
+      />
+
+      {description.isDirty && description.isEmpty && <span style={{ color: "#CC1F1F" }}>Field cannot be empty</span>}
+      <Description
+        label="Description"
+        placeholder="Type claim description"
+        className="form-description__textarea"
+        onChangeDescription={(e) => setForm({ ...form, description: e.target.value })}
+        value={form.description}
+        onChangeValid={(e) => description.onChange(e)}
+        onBlur={(e) => description.onBlur(e)}
+        disabled={role.slug === "admin" ? true : false}
+      />
+
+      {role.slug === "work" &&
+        <Button value="Cansel" type="cansel" className="form-cansel__btn" />
+      }
+      {role.slug === "work" &&
+        <Button
+          value="Create"
+          type="create"
+          className="form-create__btn"
+          disabled={!title.inputValid || !type.inputValid || !description.inputValid}
+          onCreatedClick={() => { onCreatedClick(form); }}
+        />
+      }
+    </>
+  );
 };
--- src/components/Element/Forms/ClaimForm/Select.jsx
+++ src/components/Element/Forms/ClaimForm/Select.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 
-import "./ClaimForm.scss"
-import { IconArrowDown } from "../../../../assets/img/index"
+import "./ClaimForm.scss";
+import {IconArrowDown} from "../../../../assets/img/index";
 
-export const Select = ({ onChangeType, value, label, className, onChangeValid, onBlur }) => {
-  return (
-    <div className="form-type">
-      <label className="form-type__label">
-        {label}:
-        <div className="select">
-          <select
-            className={className}
-            id="type"
-            value={value}
-            onChange={e => { onChangeType(e); onChangeValid(e) }}
-            onBlur={onBlur}
-          >
-            <option value="">Select type</option>
-            <option value="Hardware">Hardware</option>
-            <option value="Software">Software</option>
-            <option value="Troubleshooting">Troubleshooting</option>
-            <option value="Networking">Networking</option>
-          </select>
-          <img src={IconArrowDown} alt="" />
-        </div>
-      </label>
-    </div>
-  )
-}
+export const Select = ({onChangeType, value, label, className, onChangeValid, onBlur, options = [], disabled}) => {
+	return (
+		<div className="form-type">
+			<label className="form-type__label">
+				{label}:
+				<div className="select">
+					<select
+						className={className}
+						id="type"
+						value={value}
+						onChange={(e) => {
+							onChangeType(e);
+							onChangeValid(e);
+						}}
+						onBlur={onBlur}
+            disabled={disabled}
+					>
+						<option value="">Select type</option>
+						{options.map((item, key) => (
+							<option key={item.slug} value={item.slug}>
+								{item.name}
+							</option>
+						))}
+					</select>
+					<img src={IconArrowDown} alt="" />
+				</div>
+			</label>
+		</div>
+	);
+};
